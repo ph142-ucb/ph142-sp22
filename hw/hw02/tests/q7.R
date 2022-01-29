939e97d6aa28010f58b3507b18af18d94f6b9fe1
Kelsey MacCuish <kmaccuish@berkeley.edu>
2022-01-28 20:35:33 -0800

    update to test q7

--- hw/hw02/tests/q7.R
+++ hw/hw02/tests/q7.R
@@ -10,6 +10,6 @@ name: q7
 "
 
 test_that("p7", {
-  expect_true(p7 == 'bimodal' | p7 == 'skewed left')
+  expect_true(p7 == 'bimodal' | p7 == 'skewed right')
   print("Checking: p7 is one of 2 possible correct choices")
 })
